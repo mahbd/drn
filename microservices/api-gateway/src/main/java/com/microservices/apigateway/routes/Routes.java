b6c8a52923f28e179c7ebbf4d0117b3f087b31c2
Mahmudul Alam <mahmudula2000@gmail.com>
2024-06-30 13:53:24 +0600

    added basic gateway service

---
+++ microservices/api-gateway/src/main/java/com/microservices/apigateway/routes/Routes.java
@@ -0,0 +1,20 @@
+package com.microservices.apigateway.routes;
+
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+@Configuration
+public class Routes {
+    @Bean
+    public RouterFunction<ServerResponse> userServiceRoute() {
+        return GatewayRouterFunctions.route("user-service")
+                .route(RequestPredicates.path("/api/users/**"), HandlerFunctions.http("http://localhost:8081"))
+                .build();
+    }
+}
