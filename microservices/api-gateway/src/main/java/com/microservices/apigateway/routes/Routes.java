67f5ef9f4efa856f2d8b2918f38cbb5f5d7f0f1c
Mahmudul Alam <mahmudula2000@gmail.com>
2024-07-04 16:24:13 +0600

    added alert-service in api-gateway

--- microservices/api-gateway/src/main/java/com/microservices/apigateway/routes/Routes.java
+++ microservices/api-gateway/src/main/java/com/microservices/apigateway/routes/Routes.java
@@ -4,7 +4,6 @@ import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctio
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
@@ -13,8 +12,9 @@ import org.springframework.web.servlet.function.ServerResponse;
 public class Routes {
     @Bean
     public RouterFunction<ServerResponse> userServiceRoute() {
-        return GatewayRouterFunctions.route("user-service")
+        return GatewayRouterFunctions.route("routing-service")
                 .route(RequestPredicates.path("/api/users/**"), HandlerFunctions.http("http://localhost:8081"))
+                .route(RequestPredicates.path("/api/alerts/**"), HandlerFunctions.http("http://localhost:8082"))
                 .build();
     }
 }
